fc4d514c7d7b034a7d493aacc4474ace80196cbc
Ellen-code <ellenprado.pimentel@gmail.com>
2021-03-23 11:27:47 -0300

    Primeiro commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var conversorEmDolar = parseFloat(prompt("Qual valor você quer converter?"))
+
+var valorEmReal = conversorEmDolar * 5.50
+
+var valorRealFixado = valorEmReal.toFixed(2)
+
+alert(valorRealFixado)
